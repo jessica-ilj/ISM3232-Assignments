5a96a5f91fba0021028fbff850f9efefb37c8b9d
Jessica Ilok <jilok@usf.edu>
2024-08-31 11:55:48 -0400

    Task 2 - Product Inventory Management

--- cc_1.js
+++ cc_1.js
@@ -7,3 +7,14 @@ var isActive = true;
 console.log(employeeName, typeof(employeeName));
 console.log(employeeID, typeof(employeeID));
 console.log(isActive, typeof(isActive));
+
+//Task 2: Compound Data Types
+
+let products = ["Fanta Soda", "Nutter Butter cookies", "Bananas"];
+const productDetails = {
+    name: products,
+    price: 35.20,
+    inStock: true,
+
+}
+console.log(products, productDetails);
