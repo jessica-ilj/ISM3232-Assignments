c4420b2fb8d8f19a34a714ec984c562f5209a755
Jessica Ilok <jilok@usf.edu>
2024-08-31 11:04:11 -0400

    Task 1 - Employee Information

---
+++ cc_1.js
@@ -0,0 +1,9 @@
+//Task 1: Variables and Data Types
+
+let employeeName = "Harrison Lloyd"
+const employeeID = "0123"
+var isActive = true;
+
+console.log(employeeName, typeof(employeeName));
+console.log(employeeID, typeof(employeeID));
+console.log(isActive, typeof(isActive));
